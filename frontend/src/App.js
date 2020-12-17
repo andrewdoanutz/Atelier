243d31abf9860047d4f262d585ccd17ccf558538
ChrisDiep <chrisldiep@gmail.com>
2020-12-17 00:40:16 -0800

    Removed in-line styling from div in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,7 +20,7 @@ function App() {
     <div>
       <BrowserRouter>
         <div>
-          <div className = 'landing' style={window.location.pathname === '/' ? {height: "100%"} : {}}>
+          <div className = 'landing'>
           <Route exact path="/" component={Landing} />
           </div>
           <Route exact path="/account" component={Account} />
