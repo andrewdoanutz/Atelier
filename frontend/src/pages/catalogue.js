318a7452678c14507b72e830e34ca7960a12b56c
Andrew Doan <andrewdoancs@gmail.com>
2021-06-11 18:54:16 -0700

    mannequin stub done

--- frontend/src/pages/catalogue.js
+++ frontend/src/pages/catalogue.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
+import { Col, Row, Container} from "react-bootstrap"
+
 import NavBar from '../components/navbar'
 import CatalogueGrid from '../components/catalogueGrid'
 import CatalogueDetails from '../components/catalogueDetails'
-import { Col, Row, Button, Container} from "react-bootstrap"
+import UploadClothesButton from "../components/uploadClothesButton"
 
 import s1 from "../images/shirt1.jpeg"
 import s2 from "../images/shirt2.jpeg"
@@ -37,11 +39,7 @@ export default class Catalogue extends Component {
                     <Col><CatalogueDetails details={this.state.details}/></Col>
                     <Col md={8}>
                         <CatalogueGrid clothes={this.state.clothes} changeDetailsView={this.changeDetailsView.bind(this)}/>
-                        <Button className="catalogueFloatingButton">
-                            <svg id="catalogueFloatingButtonImage" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
-                                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-                            </svg>
-                        </Button>
+                        <UploadClothesButton />
                     </Col>
                 </Row>
             </Container>
--- frontend/src/pages/outfit.js
+++ frontend/src/pages/outfit.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react'
 import NavBar from '../components/navbar'
+import { Col, Row, Button, Container} from "react-bootstrap"
+
+import UploadClothesButton from "../components/uploadClothesButton"
+import OutfitMannequin from "../components/outfitMannequin"
+
+import s1 from "../images/shirt1.jpeg"
+import j1 from "../images/j1.jpeg"
+import sh1 from "../images/shoe1.jpeg"
+
 
 export default class Outfit extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      topPic: s1,
+      pantsPic: j1,
+      shoesPic: sh1,
+
+    }
+  }
   render() {
     return (
       <>
       <NavBar/>
-        <div>outfit</div>
-        <div>outfit</div>
-        <div>outfit</div>
-        <div>outfit</div>
-        <div>outfit</div>
-        <div>outfit</div>
-        <div>outfit</div>
-        <div>outfit</div>
-        <div>outfit</div>
+      <Container style={{paddingTop:"10%"}}>
+          <Row>
+              <Col md={8}></Col>
+              <Col>
+                <OutfitMannequin topPic={this.state.topPic} pantsPic={this.state.pantsPic} shoesPic={this.state.shoesPic} />
+                <UploadClothesButton/>
+              </Col>
+          </Row>
+      </Container>
       </>
     )
   }
