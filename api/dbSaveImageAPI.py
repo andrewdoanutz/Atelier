288046453decede6f3ea026d2be33bbac9de72fd
Andrew Doan <andrewdoancs@gmail.com>
2022-02-12 15:29:24 -0800

    working change category

--- api/dbSaveImageAPI.py
+++ api/dbSaveImageAPI.py
@@ -36,3 +36,24 @@ class DatabaseSaveImageAPI(DatabaseAPI):
     self.db.conn.close()
 
     return jsonify(status = "success")
+
+  def patch(self):
+    parser = reqparse.RequestParser()
+    parser.add_argument("email", type=str)
+    parser.add_argument("category", type=str)
+    parser.add_argument("image", type=str)
+
+    args = parser.parse_args()
+    user = self.db.conn.execute("SELECT * FROM USERS WHERE email=:email",
+     {"email": args["email"]}).fetchall()
+    if not user:
+      self.db.conn.close()
+      print("User not found:", args["email"])
+      return make_response(jsonify(error = "User does not exist"), 404)
+
+    self.db.conn.execute("UPDATE IMAGES SET label=:label WHERE user_email=:email AND image=:image", {"email": args["email"], "label": args["category"], "image": args["image"]})
+
+    self.db.conn.commit()
+    self.db.conn.close()
+
+    return jsonify(status = "success")
--- frontend/src/components/catalogueDetails.js
+++ frontend/src/components/catalogueDetails.js
@@ -1,60 +1,89 @@
 import React, { Component } from 'react'
 import {Col, Row, Card, Container, Button, Form} from "react-bootstrap"
+import axios from 'axios';
+import Cookies from 'universal-cookie';
 
 export default class CatalogueDetails extends Component {
-  render() {
-    return (
-    <Row>
-        <Col>
-            <Card className="shadow" style={{padding: "0%", position: "fixed", borderColor: "#EF9A9A"}}>
-                <Card.Body>
-                    <Container>
-                    <Row>
-                        <Col>
-                            <Row><img className="catalogueDetailsImage" src={this.props.details["pic"]}/></Row>
-                            <Row style={{paddingTop:"5%"}}>
-                                <Card style={{ width: "100%", textAlign: "left", margin:"0", borderColor: "#C4C4C4"}}>
-                                    <Card.Body>
-                                    <Card.Title as="h6">Details</Card.Title>
-                                    <Container>
-                                        <Form>
-                                            <Form.Group>
-                                                <Form.Select id="catalogueDetailsCategory">
-                                                    <option value="T-shirt/top" selected={"T-shirt/top" === this.props.details["details"]["category"] ? true : false}>T-shirt/top</option>
-                                                    <option value="Trouser" selected={"Trouser" === this.props.details["details"]["category"] ? true : false}>Trouser</option>
-                                                    <option value="Pullover" selected={"Pullover" === this.props.details["details"]["category"] ? true : false}>Pullover</option>
-                                                    <option value="Dress" selected={"Dress" === this.props.details["details"]["category"] ? true : false}>Dress</option>
-                                                    <option value="Coat" selected={"Coat" === this.props.details["details"]["category"] ? true : false}>Coat</option>
-                                                    <option value="Sandal" selected={"Sandal" === this.props.details["details"]["category"] ? true : false}>Sandal</option>
-                                                    <option value="Shirt" selected={"Shirt" === this.props.details["details"]["category"] ? true : false}>Shirt</option>
-                                                    <option value="Sneaker" selected={"Sneaker" === this.props.details["details"]["category"] ? true : false}>Sneaker</option>
-                                                    <option value="Bag" selected={"Bag" === this.props.details["details"]["category"] ? true : false}>Bag</option>
-                                                    <option value="Ankle boot" selected={"Ankle boot" === this.props.details["details"]["category"] ? true : false}>Ankle boot</option>
-                                                </Form.Select>
-                                            </Form.Group>
-                                        </Form>
-                                    </Container>
-                                    </Card.Body>
-                                </Card>
-                            </Row>
-                            <Row style={{paddingTop:"5%"}}>
-                                <Col md={3}>
-                                </Col>
-                                <Col md={8}>
-                                    <Container>
-                                        <Button className="saveDetailsButton">Save</Button>
-                                    </Container>
-                                </Col>
-                                <Col md={2}>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
-                    </Container>
-                </Card.Body>
-            </Card>
-        </Col>
-    </Row>
-    )
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+          category: this.props.details["details"]["category"],
+        }
+        this.cookies = new Cookies();
+        this.onChangeDetails = this.onChangeDetails.bind(this)
+        this.detailsUpdateOnClick = this.detailsUpdateOnClick.bind(this)
+    }
+
+    detailsUpdateOnClick(){
+        axios.patch('http://localhost:5000/api/db/saveimage', {category: this.state.category, email: this.cookies.get("email"), image: this.props.details["pic"]}).then(response => {
+            console.log("SUCCESS", response)
+            }).catch(error => {
+                console.log(error)
+            }).finally(() => {
+                // window.location.reload(false);
+            })
+    }
+    
+    onChangeDetails(event){
+        this.setState({
+            category: event.target.value
+        })
+    }
+
+    render() {
+        console.log(this.props.details["details"]["category"])
+        return (
+        <Row>
+            <Col>
+                <Card className="shadow" style={{padding: "0%", position: "fixed", borderColor: "#EF9A9A"}}>
+                    <Card.Body>
+                        <Container>
+                        <Row>
+                            <Col>
+                                <Row><img className="catalogueDetailsImage" src={this.props.details["pic"]}/></Row>
+                                <Row style={{paddingTop:"5%"}}>
+                                    <Card style={{ width: "100%", textAlign: "left", margin:"0", borderColor: "#C4C4C4"}}>
+                                        <Card.Body>
+                                        <Card.Title as="h6">Details</Card.Title>
+                                        <Container>
+                                            <Form>
+                                                <Form.Group>
+                                                    <Form.Select id="catalogueDetailsCategory" value={this.props.details["details"]["category"]} onChange={this.onChangeDetails}>
+                                                        <option value="T-shirt/top" >T-shirt/top</option>
+                                                        <option value="Trouser" >Trouser</option>
+                                                        <option value="Pullover" >Pullover</option>
+                                                        <option value="Dress" >Dress</option>
+                                                        <option value="Coat" >Coat</option>
+                                                        <option value="Sandal" >Sandal</option>
+                                                        <option value="Shirt" >Shirt</option>
+                                                        <option value="Sneaker" >Sneaker</option>
+                                                        <option value="Bag" >Bag</option>
+                                                        <option value="Ankle boot" >Ankle boot</option>
+                                                    </Form.Select>
+                                                </Form.Group>
+                                            </Form>
+                                        </Container>
+                                        </Card.Body>
+                                    </Card>
+                                </Row>
+                                <Row style={{paddingTop:"5%"}}>
+                                    <Col md={3}>
+                                    </Col>
+                                    <Col md={8}>
+                                        <Container>
+                                            <Button className="saveDetailsButton" onClick={this.detailsUpdateOnClick}>Save</Button>
+                                        </Container>
+                                    </Col>
+                                    <Col md={2}>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                        </Container>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
+        )
+    }
 }
